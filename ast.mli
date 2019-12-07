f02a78e920aa8a5ac93b3a8c3f874f8d7fe5d104
guisado_asmatico <raquel.guerra2403@gmail.com>
2019-12-07 12:55:23 +0000

    Adicionados tipos
    Foram adicionados:
    * Tipo ident - para identificadores;
    * Tipo unop - para as operações de negação e números negativos:
    * Tipo binop - para todas as operações binárias e aritméticas

--- ast.mli
+++ ast.mli
@@ -1,3 +1,20 @@
+
+(* tipo Identificadores *)
+type ident = string
+
+
+(* Constantes *)
 type constant = 
   | Cbool of bool
   | Cint of int
+
+(* Operadores unários *)
+type unop =
+  | Uneg
+  | Unot
+
+
+type binop =
+  | Badd | Bsub | Bmul | Bdiv
+  | Beq  | Bneq | Bgt | Blt | Bleq | Bgeq
+  | Band | Bor
